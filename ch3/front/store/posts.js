9eed9803e6bdc8570b10b30fb80cb8ee74ddcf6e
inch <sxin2949@naver.com>
2020-03-05 22:57:37 +0900

    add function infinite scrolling to posts.js

--- ch3/front/store/posts.js
+++ ch3/front/store/posts.js
@@ -1,6 +1,9 @@
 export const state = () => ({
-  mainPosts: []
+  mainPosts: [],
+  hasMorePost: true // 이게 false이면 서버에서 게시글을 더 이상 불러오지 않음.
 });
+
+const limit = 10;
 
 export const mutations = {
   addMainPost(state, payload) {
@@ -13,6 +16,22 @@ export const mutations = {
   addComment(state, payload) {
     const index = state.mainPosts.findIndex(v => v.id === payload.postId);
     state.mainPosts[index].Comments.unshift(payload);
+  },
+  loadPosts(state, payload) {
+    const fakePosts = Array(limit)
+      .fill()
+      .map(v => ({
+        id: Math.random().toString(),
+        User: {
+          id: 1,
+          nickname: "제로초"
+        },
+        content: `Hello infinite scrolling ~ ${Math.random()}`,
+        Comment: [],
+        Images: []
+      }));
+    state.mainPosts = state.mainPosts.concat(fakePosts);
+    state.hasMorePost = fakePosts.length === limit; // 만약 limit이랑 길이가 맞지 않으면 false가 되므로 hasMorePost 값에 false를 넣는 거임.
   }
 };
 
@@ -28,5 +47,8 @@ export const actions = {
   addComment({ commit }, payload) {
     // 댓글 달기
     commit("addComment", payload);
+  },
+  loadPosts({ commit, state }, payload) {
+    commit("loadPosts");
   }
 };
