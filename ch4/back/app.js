14c6331c3ec472dbd74b10e25191b0643873958e
inch <sxin2949@naver.com>
2020-04-25 23:57:13 +0900

    update sequlize version

--- ch4/back/app.js
+++ ch4/back/app.js
@@ -3,9 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("안녕 ㅎㅎz");
+  res.send("안녕 ㅎㅎ");
 });
 
 app.listen(3085, () => {
   console.log(`백엔드 ${3085}번 포트에서 작동 중.`);
 });
+
+//sequelize
